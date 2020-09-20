fbd741315651525217d4f170c04ef8f1714c03cd
306093226wanggitgitgitgit <306093226@qq.com>
2020-09-20 23:39:43 +0800

    ArgsTest中增加should_return_default_value_when_new_Args_given_string_and_schema

--- src/test/java/com/thoughtworks/basic/ArgsTest.java
+++ src/test/java/com/thoughtworks/basic/ArgsTest.java
@@ -97,4 +97,50 @@ public class ArgsTest {
         //when
         String a = args.getValueOf("a");
     }
+
+    @Test
+    public void should_return_a_is_not_defined_when_new_Args_given_string_and_schema() {
+        //given
+        String argsText = "-l true -p 8080 -d usr/logs";
+        Set<FlagSchema> flagSchemas = new HashSet<>();
+        flagSchemas.add(new FlagSchema("l", Boolean.TYPE));
+        flagSchemas.add(new FlagSchema("d", String.class));
+        flagSchemas.add(new FlagSchema("p", Integer.class));
+        Schema schema = new Schema(flagSchemas);
+        Args args = new Args(argsText, schema);
+
+        //when
+        String errorStr = "";
+        try {
+            String a = args.getValueOf("a");
+        } catch (Exception e) {
+            errorStr = e.getMessage();
+        }
+
+        //then
+        Assert.assertEquals("a is not find!", errorStr);
+    }
+
+    @Test
+    public void should_return_default_value_when_new_Args_given_string_and_schema() throws Exception {
+        //given
+        String argsText = "-l true";
+        Set<FlagSchema> flagSchemas = new HashSet<>();
+        flagSchemas.add(new FlagSchema("l", Boolean.TYPE));
+        flagSchemas.add(new FlagSchema("d", String.class));
+        flagSchemas.add(new FlagSchema("p", Integer.class));
+        Schema schema = new Schema(flagSchemas);
+        Args args = new Args(argsText, schema);
+
+        //when
+        String l = args.getValueOf("l");
+        String p = args.getValueOf("p");
+        String d = args.getValueOf("d");
+
+
+        //then
+        Assert.assertEquals("true", l);
+        Assert.assertEquals("0", p);
+        Assert.assertEquals("", d);
+    }
 }
