8e7e2ea815d7c4ff44128497a813cd7762b66076
306093226wanggitgitgitgit <306093226@qq.com>
2020-09-20 22:24:03 +0800

    修改Arg类名

--- src/main/java/ConsoleCommandAnalysis/Arg.java
+++ src/main/java/ConsoleCommandAnalysis/Arg.java
@@ -2,21 +2,29 @@ package ConsoleCommandAnalysis;
 
 import java.util.Objects;
 
-public class KeyValuePair {
+public class Arg {
     private final String key;
     private final String value;
 
-    public KeyValuePair(String key, String value) {
+    public Arg(String key, String value) {
 
         this.key = key;
         this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        KeyValuePair that = (KeyValuePair) o;
+        Arg that = (Arg) o;
         return Objects.equals(key, that.key) &&
                 Objects.equals(value, that.value);
     }
@@ -24,5 +32,13 @@ public class KeyValuePair {
     @Override
     public int hashCode() {
         return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Arg{" +
+                "key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
     }
 }
